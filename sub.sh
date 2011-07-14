9724f16fbc509daf0e7248e570c4a260a572ab54
docteurklein <florian.klein@free.fr>
2011-07-14 20:03:39 +0200

    renamed dot files without dot

---
+++ sub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for cfg in `git config -f .gitmodules -l`
+do
+    url=`echo $cfg | awk -F"submodule.*.url=" '{print $2}'`;
+
+    if [ -n "$url" ]; then 
+
+        echo "git submodule add $url $path";
+        git submodule add $url $path;
+
+        url="";
+    else
+        path=`echo $cfg | awk -F"submodule.*.path=" '{print $2}'`;
+    fi
+done
