615b70c433c7f70527d904dbe2fc42daa76380a9
Klein Florian <florian.klein@free.fr>
2012-08-20 11:57:36 +0200

    add temaocil config

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -s dot-files/vim           ~/.vim
 ln -s dot-files/vimrc         ~/.vimrc
 
 ln -s dot-files/tmux.conf     ~/.tmux.conf
-ln -s dot-files/tmux.rc       ~/tmux.rc
+ln -s dot-files/teamocil      ~/.teamocil
 
 ln -s dot-files/gitconfig     ~/.gitconfig
 ln -s dot-files/gitignore     ~/.gitignore
